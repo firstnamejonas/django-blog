26109a4814e66e7af5c33b1e35e0352fb7f7848e
Jonas Hessling <firstname.jonas@icloud.com>
2024-02-06 16:20:38 +0000

    Add meta class

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,11 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    class Meta:
+        ordering = ["-created_on"]
+        
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(
