d9fcda2293aea1427f3ba1f84c4f45d6d88a69ee
Jonas Hessling <firstname.jonas@icloud.com>
2024-02-07 13:45:36 +0000

    Debug files & make migrations

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
+
+# Create your models here.
 
 
 class Post(models.Model):
@@ -15,6 +16,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-created_on"]
@@ -22,20 +24,15 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
+
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        related_name="comments"
-    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="comments_author"
-    )
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["created_on"]
